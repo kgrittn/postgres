273487cff027849aebb3c5260f5739df08dcad6e
Kevin Grittner <kgrittn@postgresql.org>
2013-01-02 09:29:25 -0600

    Revert change to return type of InsertRule; it is now used.

--- src/backend/rewrite/rewriteDefine.c
+++ src/backend/rewrite/rewriteDefine.c
@@ -50,7 +50,7 @@ static void setRuleCheckAsUser_Query(Query *qry, Oid userid);
  *	  takes the arguments and inserts them as a row into the system
  *	  relation "pg_rewrite"
  */
-static void
+static Oid
 InsertRule(char *rulname,
 		   int evtype,
 		   Oid eventrel_oid,
