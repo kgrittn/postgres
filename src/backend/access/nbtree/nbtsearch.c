32281329d9e4a6ce91e969e6e18c2e586ac38e4b
Kevin Grittner <kgrittn@postgresql.org>
2015-02-10 08:41:40 -0600

    Keep btree buffer pins during index-only scans.
    We must either do this or check visibility of heap pages before
    releasing the read lock on the index page.  Taking the easy way for
    now; if we see these pins stall vacuums in practice, we may want to
    go the other way with it.

--- src/backend/access/nbtree/nbtsearch.c
+++ src/backend/access/nbtree/nbtsearch.c
@@ -54,12 +54,13 @@ _bt_drop_lock_and_maybe_pin(IndexScanDesc scan, BTScanPos sp)
 {
 	LockBuffer(sp->buf, BUFFER_LOCK_UNLOCK);
 
- 	if (IsMVCCSnapshot(scan->xs_snapshot) &&
- 		RelationNeedsWAL(scan->indexRelation))
- 	{
- 		ReleaseBuffer(sp->buf);
- 		sp->buf = InvalidBuffer;
- 	}
+	if (IsMVCCSnapshot(scan->xs_snapshot) &&
+		RelationNeedsWAL(scan->indexRelation)
+		!scan->xs_want_itup)
+	{
+		ReleaseBuffer(sp->buf);
+		sp->buf = InvalidBuffer;
+	}
 }
 
 
