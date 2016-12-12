e8399c610a63b6ba6547633d83c46e0eebfb3e16
Kevin Grittner <kgrittn@postgresql.org>
2016-12-12 08:53:28 -0600

    Move definitions of Tsr and Tsrmd to queryenvironment.h.
    At least for now, these structures are only needed when using a
    QueryEnvironment.  This is in preparation for renaming these
    structures.

--- src/include/utils/queryenvironment.h
+++ src/include/utils/queryenvironment.h
@@ -14,10 +14,33 @@
 #ifndef QUERYENVIRONMENT_H
 #define QUERYENVIRONMENT_H
 
+#include "access/tupdesc.h"
 #include "utils/tuplestore.h"
+
+
+typedef struct TsrmdData
+{
+	char			   *name;		/* name used to identify the tuplestore */
+	TupleDesc			tupdesc;	/* description of result rows */
+} TsrmdData;
+
+typedef TsrmdData *Tsrmd;
+
+/*
+ * Tuplestore Relation data; used for parsing named tuplestores, like
+ * transition tables in AFTER triggers.
+ */
+typedef struct TsrData
+{
+	TsrmdData	md;
+	Tuplestorestate	   *tstate;		/* data (or tids) */
+} TsrData;
+
+typedef TsrData *Tsr;
 
 /* This is an opaque structure outside of queryenvironment.c itself. */
 typedef struct QueryEnvironment QueryEnvironment;
+
 
 extern QueryEnvironment *create_queryEnv(void);
 extern Tsrmd get_visible_tuplestore_metadata(QueryEnvironment *queryEnv, const char *refname);
